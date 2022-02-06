03d75b9c2b1421d0bff6f0c1d774a9d9b340a6d7
saurabhmvc <saurabh.mvc@gmail.com>
2022-02-06 14:42:09 +0530

    Phase 1

---
+++ SimpleApi/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleApi
+{
+public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "Saurabh";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Test/SimpleApi.Tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using System;
+using Xunit;
+using SimpleApi;
+
+namespace SimpleApi.Tests
+{
+    public class UnitTest1
+    {
+        ValuesController objValuesController = new ValuesController();
+
+        [Fact]
+        public void Test1()
+        {
+            var temp = objValuesController.Get(1);
+            Assert.Equal("Saurabh", temp);
+        }
+    }
+}
